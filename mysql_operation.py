a5d541cdebf00697db2e45f86411821551f8239b
Roger <luojie.5408@163.com>
2018-02-26 18:18:49 +0800

    lalala

--- mysql_operation.py
+++ mysql_operation.py
@@ -11,7 +11,7 @@ def connect2Mysql():
 
 def readNotes():
     conn, cursor = connect2Mysql()
-    cursor.execute('select * from flow_item')
+    cursor.execute('SELECT * from flow_item')
     values = cursor.fetchall()
     cursor.close()
     conn.close()
@@ -20,8 +20,35 @@ def readNotes():
 
 def readNotesById(item_id):
     conn, cursor = connect2Mysql()
-    cursor.execute('select * from flow_item where item_id = %s', (item_id,))
+    cursor.execute('SELECT * from flow_item where item_id = %s', (item_id,))
     values = cursor.fetchall()
     cursor.close()
     conn.close()
     return values
+
+
+def deleteNote(item_id):
+    conn, cursor = connect2Mysql()
+    cursor.execute('SELECT * from flow_item where item_id = %s', (item_id,))
+    values = cursor.fetchall()
+    if len(values) != 0: # find none
+        cursor.execute('DELETE from flow_item where item_id = %s', (item_id,))
+        conn.commit()        
+    cursor.close()
+    conn.close()
+
+
+def touchNote(item_id, content):
+    conn, cursor = connect2Mysql()
+    cursor.execute('SELECT * from flow_item where item_id = %s', (item_id,))
+    values = cursor.fetchall()
+    if len(values) == 0: # find none
+        cursor.execute('INSERT into flow_item (content, item_id) values (%s, %s)', [content, item_id])
+    else:# find one
+        cursor.execute('UPDATE flow_item set content = %s where item_id = %s', [content, item_id])
+    # insert_id = cursor.lastrowid
+    conn.commit()        
+    cursor.close()
+    conn.close()
+
+
--- server.py
+++ server.py
@@ -3,46 +3,47 @@ from flask_cors import *
 from flask import Flask, render_template, Response, request
 import json
 import mysql_operation as sql
-from machine_learning.findSimilar import findWord
+from w2v.findSimilar import findWord
 
 app = Flask(__name__, static_folder='static')
 CORS(app, supports_credentials=True)
 
-@app.route('/search/<word>', methods=['POST', 'GET'])
+
+@app.route('/search/<word>', methods=['GET'])
 def search(word):
-    if request.method == 'GET':
-        found_list = findWord()
-        
-        if len(notes) != 0:
-            return json.dumps(notes)
-        else:
-            return json.dumps([])
+    found_list = findWord(word,20)
+    if len(found_list) != 0:
+        return json.dumps(found_list)
+    else:
+        return json.dumps([])
 
 
-@app.route('/notes', methods=['POST', 'GET'])
+@app.route('/notes', methods=['GET'])
 def notes():
-    if request.method == 'GET':
-        notes = sql.readNotes()
-        if len(notes) != 0:
-            return json.dumps(notes)
-        else:
-            return json.dumps([])
+    notes = sql.readNotes()
+    if len(notes) != 0:
+        return json.dumps(notes)
+    else:
+        return json.dumps([])
 
 
-@app.route('/note/<item_id>', methods=['POST', 'GET'])
+@app.route('/note/<item_id>', methods=['POST', 'PUT', 'DELETE'])
 def note(item_id):
-    if request.method == 'GET':
-        notes = sql.readNotesById(item_id)
-        if len(notes) != 0:
-            return json.dumps(notes[0])
-        else:
-            return json.dumps({})
-
-    if request.method == 'POST':
-        request.form['content']
-
-    return json.dumps('not found')
+    if (request.method == 'POST') or (request.method == 'PUT'):
+        content = request.form['content']
+        sql.touchNote(item_id, content)
+    if request.method == 'DELETE':
+        sql.deleteNote(item_id)
+    return json.dumps('ok')
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)  # ,  debug=True
+
+
+# if request.method == 'GET':
+#     notes = sql.readNotesById(item_id)
+#     if len(notes) != 0:
+#         return json.dumps(notes[0])
+#     else:
+#         return json.dumps({})
