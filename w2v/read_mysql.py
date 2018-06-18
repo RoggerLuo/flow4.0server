83114ec9bbeb32c3db692e9158b9e5f7f117ee51
Roger <luojie.5408@163.com>
2018-06-18 23:29:50 +0800

    update

--- w2v/read_mysql.py
+++ w2v/read_mysql.py
@@ -3,10 +3,11 @@ import mysql.connector
 from task import Task
 import os
 import json
-import time    
+import time
 
 c = Config()
 t = Task(c)
+
 
 def connect2Mysql():
     conn = mysql.connector.connect(
@@ -17,15 +18,27 @@ def connect2Mysql():
 
 def readNotes():
     conn, cursor = connect2Mysql()
-    cursor.execute('SELECT * from flow_item where status = 0 Order By modify_time Desc')
+    cursor.execute(
+        'SELECT * from flow_item where status = 0 Order By modify_time Desc')
     values = cursor.fetchall()
 
     cursor.execute('UPDATE temp set value = 0 where name = %s', ('has_new',))
-    conn.commit()        
+    conn.commit()
 
     cursor.close()
     conn.close()
     return values
+
+
+def readDeletedNotes():
+    conn, cursor = connect2Mysql()
+    cursor.execute(
+        'SELECT * from flow_item where status = 1 Order By modify_time Desc')
+    values = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return values
+
 
 def ifNewNoteComing():
     conn, cursor = connect2Mysql()
@@ -44,19 +57,19 @@ for x in range(20000):
     notes_counts = len(notes)
     cost = 0
     for i in range(notes_counts):
-        if ifNewNoteComing() == 1: break                                        
+        if ifNewNoteComing() == 1:
+            break
         wordlist = json.loads(notes[i][3])
-        cost += t.feedlist(wordlist)/1000
+        cost += t.feedlist(wordlist) / 1000
     t.save()
 
-    if x%4 == 0:
+    if x % 4 == 0:
         for j in range(len(deletedNote)):
             wordlist = json.loads(deletedNote[i][3])
             t.feedlist(wordlist)
 
-
-    print('[第%d轮,耗时%f分],cost:%f' % (x,(startTime-time.time())/60,cost))
-    if abs(cost - lastCost)/lastCost <= 0.02:
+    print('[第%d轮,耗时%f分],cost:%f' % (x, (startTime - time.time()) / 60, cost))
+    if abs(cost - lastCost) / lastCost <= 0.02:
         c.rateChange(0.01)
         print('rate change 0.01')
     else:
